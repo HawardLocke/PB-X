d1d9ecfd582e7d1b5309594152b726659d9e98d4
HawardLocke <play46917>
2016-11-06 00:35:24 +0800

    pbx

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+# In each proto file, the max count of message defines.
+MAX_ID_COUNT_PER_FILE = 1000
+
+# Name space / module of MsgID.
+NAME_SPACE = 'PBX'
+
+# Generated c# files will be copied to this folder.
+CS_DEST_DIR = '/../LiteServer/LiteServer/Source/Protobuf/'
+
+# Generated lua files will be copied to this folder.
+LUA_DEST_DIR = '/../uLab/uLab/Assets/Lua/protocol/'
