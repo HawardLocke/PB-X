c6a904c037e205f8dc3e0a6fe839b957f9418dd1
HawardLocke <1534921818@qq.com>
2016-11-21 16:29:08 +0800

    protobuf-net

--- settings.py
+++ settings.py
@@ -4,12 +4,17 @@ MAX_ID_COUNT_PER_FILE = 1000
 
 # Name space / module of MsgID.
 NAME_SPACE = 'PBX'
+# Name space of c# MsgID.
+NAME_SPACE_CS = 'Lite'
 
 # Auto copy generated file to dest folders.
 AUTO_COPY_FILE = True
 
+# use protobuf-net or only use google.protobuf to generate cs files.
+USE_PROTOBUF_NET = True
+
 # Generated c# files will be copied to this folder.
-CS_DEST_DIR = '/../LiteServer/LiteServer/Source/Logic/Protocol/'
+CS_DEST_DIR = ['/../LiteServer/LiteServer/Source/Logic/Protocol/auto_gen/', '/../uLab/uLab/Assets/Scripts/Network/Protocol/auto_gen/']
 
 # Generated lua files will be copied to this folder.
 LUA_DEST_DIR = '/../uLab/uLab/Assets/Lua/protocol/'
